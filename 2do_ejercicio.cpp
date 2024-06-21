af3e5698169e7e20900e33d38960936fc4618d4f
Elvis-199afk <earocutipaq@unjbg.edu.pe>
2024-06-20 21:57:04 -0500

    Agregar 2do ejercicio de estructuras selectivas

---
+++ 2do_ejercicio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	float valor_compra, valor_final, descuento;
+	cout << "Introduzca el valor de la compra: ";
+	cin >> valor_compra;
+	if (valor_compra > 1000){
+		descuento = valor_compra*0.2;
+		valor_final = valor_compra - descuento;
+		cout << "El valor final de la compra es: " << valor_final << endl;
+	} else {
+		valor_final = valor_compra;
+		cout << "El valor final de la compra es: " << valor_final << endl;
+	}
+	return 0;
+}
