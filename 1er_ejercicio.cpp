a7b889c702564ac381e30aecc63242d7a68eb927
Elvis-199afk <earocutipaq@unjbg.edu.pe>
2024-06-20 21:52:20 -0500

    Agregando validacion de numeros

--- 1er_ejercicio.cpp
+++ 1er_ejercicio.cpp
@@ -4,17 +4,41 @@ using namespace std;
 int main (){
 	
 	float c1, c2, c3, promedio;
-	cout << "Introduce la primera calificacion: ";
-	cin >> c1;
-	cout << "Introduce la segunda calificacion: ";
-	cin >> c2;
-	cout << "Introduce la tercera calificacion: ";
-	cin >> c3;
+	
+	do{
+		
+		cout << "Introduce la primera calificacion: ";
+		cin >> c1;
+		if(c1<0){
+			cout<<"Introduzca numeros positivos"<<endl;
+			
+		}
+	}while(c1<0);
+	
+	do{
+		
+		cout << "Introduce la segunda calificacion: ";
+		cin >> c2;
+		if(c2<0){
+			cout<<"Introduzca numeros positivos"<<endl;
+			
+		}
+	}while(c2<0);
+	
+	do{
+		
+		cout << "Introduce la tercera calificacion: ";
+		cin >> c3;
+		if(c3<0){
+			cout<<"Introduzca numeros positivos"<<endl;
+		}
+	}while(c3<0);
+	system("cls");
 	promedio = (c1+c2+c3)/3;
 	if (promedio >= 70){
-		cout << "Aprobado.";
+		cout << "===Aprobado==="<<endl;
 	} else {
-		cout << "Reprobado.";
+		cout << "===Reprobado==="<<endl;
 	}
 	return 0;
 
